7124644d8ea13fc440acce506e65db1e8f9c3f3e
reyesandrea <seyerandreav@gmail.com>
2019-05-10 23:55:14 -0400

    ds

--- Laboratorio 2/lab2.R
+++ Laboratorio 2/lab2.R
@@ -9,33 +9,51 @@ zona3_guisar = zona3[carne$Tipo3 == "Guisar"]
 zona4 = carne$Zona4
 zona4_guisar = zona4[carne$Tipo4 == "Guisar"]
 
-# 1
-#1. Realice un análisis descriptivo de los precios de las zonas del tipo de carne de guisar. 
-#En el estudio considere histograma, boxplot y una tabla con información 
-#del mı́nimo, cuartiles, media y desviación estándar de cada zona (10 puntos).
+# PREGUNTA 1
+# Realice un análisis descriptivo de los precios de las zonas del tipo de carne de guisar. 
+# En el estudio considere histograma, boxplot y una tabla con información 
+# del mı́nimo, cuartiles, media y desviación estándar de cada zona (10 puntos).
 
 hist(zona1_guisar, main = "Histograma de precios de carne guisar en Zona 1", ylab = "Abastecimientos", xlab = "Precios en Zona 1")
 hist(zona2_guisar, main = "Histograma de precios de carne guisar en Zona 2", ylab = "Abastecimientos", xlab = "Precios en Zona 2")
 hist(zona3_guisar, main = "Histograma de precios de carne guisar en Zona 3", ylab = "Abastecimientos", xlab = "Precios en Zona 3")
 hist(zona4_guisar, main = "Histograma de precios de carne guisar en Zona 4", ylab = "Abastecimientos", xlab = "Precios en Zona 4")
 
-#Boxplot con las 4 Zonas
+# Boxplot con las 4 Zonas
 boxplot(zona1_guisar,zona2_guisar,zona3_guisar,zona4_guisar, ylab="Precios", xlab="Zonas",col=c("red","pink","gray","yellow"))
 
-#Boxplot ZONA 1
+# Boxplot ZONA 1
 
 boxplot(zona1_guisar, ylab="Precios", xlab="Zona 1", col=c("red"))
 
-#Boxplot ZONA 2
+# Boxplot ZONA 2
 
 boxplot(zona2_guisar, ylab="Precios", xlab="Zona 2", col=c("pink"))
 
-#Boxplot ZONA 3
+# Boxplot ZONA 3
 
 boxplot(zona3_guisar, ylab="Precios", xlab="Zona 3", col=c("gray"))
 
-#Boxplot ZONA 4
+# Boxplot ZONA 4
 
 boxplot(zona4_guisar, ylab="Precios", xlab="Zona 4", col=c("yellow"))
 
+######################
 
+# PREGUNTA 2
+# Determine un intervalo de confianza para la media de los precios de la carne de guisar 
+# para la Zona 1 y así mismo, para los precios de la Zona 3. (confianza de 99 %).
+
+# Intervalo de confianza Zona 1
+mean(zona1_guisar)
+t.test(zona1_guisar, conf.level = 0.99 )$conf.int
+
+# Intervalo de confianza Zona 2
+mean(zona3_guisar)
+t.test(zona3_guisar, conf.level = 0.99 )$conf.int
+
+######################
+
+# PREGUNTA 3
+# ¿Cuál de los dos alimentos logra mayor precio en la carne de guisar, la Zona 2 o la 
+# Zona 4? Suponga 1 − α = 0,99
